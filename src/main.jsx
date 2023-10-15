bd0145ae54f5aea1431e9d8139589fe5c2482d80
Mahin zaman Arnob <arnobmahin007@gmail.com>
2023-10-15 17:50:52 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
+import router from './router';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+	</React.StrictMode>
+);
